7b9b9777977258d20ec0af2550fa24d8aedff1f0
Alex Sacchetti <alexandersacchetti@gmail.com>
2023-10-31 23:08:18 -0400

    update firebase.json "public" > "build"

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
- 
-    <App />
- 
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
+
+// if npm run build is failing due to node sass issue, run the following command:
+// npm rebuild node-sass
